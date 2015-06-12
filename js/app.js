d13583645a2ed23681d3fdd4b2655b31e1527d85
Dregu <dregu@dreg.us>
2015-06-13 00:22:18 +0300

    nothing new really

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic('static')).listen(8080);
 
-var show = function(control, title, text, delay) {
+var show = function (control, title, text, delay) {
 	var data = {
 		command: 'show',
 		control: control,
@@ -14,25 +14,25 @@ var show = function(control, title, text, delay) {
 		delay: delay
 	};
 	send(data);
-}
+};
 
-var hide = function(control) {
+var hide = function (control) {
 	var data = {
 		command: 'hide',
 		control: control
 	};
 	send(data);
-}
+};
 
-var send = function(data) {
+var send = function (data) {
 	console.log(data);
-	for(var i in server.clients) {
+	for (var i in server.clients) {
 		server.clients[i].send(JSON.stringify(data));
 	}
-}
+};
 
-$(function() {
-	$('.show').click(function(e) {
+$(function () {
+	$('.show').click(function (e) {
 		console.log(this);
 		var control = $(this).parent().attr('id');
 		var title = $(this).parent().find('input').val();
@@ -40,21 +40,21 @@ $(function() {
 		console.log('show', control, title, text);
 		show(control, title, text, null);
 	});
-	$('.hide').click(function(e) {
+	$('.hide').click(function (e) {
 		var control = $(this).parent().attr('id');
 		console.log('hide', control);
 		hide(control);
 	});
-	$('#hide').click(function(e) {
+	$('#hide').click(function (e) {
 		console.log('hide');
 		hide(null);
 	});
-	$('#show').click(function(e) {
-		$('.control').each(function() {
+	$('#show').click(function (e) {
+		$('.control').each(function () {
 			var control = $(this).attr('id');
 			var title = $(this).find('input').val();
 			var text = $(this).find('textarea').val();
-			if(title == '' && text == '') {
+			if (title === '' && text === '') {
 				console.log('hide', control);
 				hide(control);
 			} else {
@@ -63,14 +63,14 @@ $(function() {
 			}
 		});
 	});
-	$('#clear').click(function(e) {
+	$('#clear').click(function (e) {
 		$('input').val('');
 		$('textarea').val('');
 	});
-	$('#hidelogo').click(function(e) {
+	$('#hidelogo').click(function (e) {
 		hide('logo');
 	});
-	$('#showlogo').click(function(e) {
+	$('#showlogo').click(function (e) {
 		show('logo');
 	});
 });
